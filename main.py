0e49188147cc70b47283e186f6f32e690993e764
asrioth <liamswords@gmail.com>
2024-12-16 16:09:17 +1300

    added a game clock to limit frame rate to 60fps

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     black_color = pygame.Color(0,0,0)
     while True:
@@ -16,6 +18,7 @@ def main():
 
         screen.fill(black_color)
         pygame.display.flip()
+        dt = (game_clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
